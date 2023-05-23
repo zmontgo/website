e3ef6035406b96d6f424826b950406966655a1a1
Zachary Montgomery <zachmontgo@iodinedev.com>
2023-05-23 12:00:54 -0500

    Auth for redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -2,4 +2,13 @@ import { Redis } from "ioredis";
 
 if (!process.env.REDIS_URL) throw new Error('Missing REDIS_URL env var');
 
-export const redis = new Redis(process.env.REDIS_URL);
+var redis;
+if (process.env.REDIS_PASSWORD) {
+  redis = new Redis(process.env.REDIS_URL, {
+    password: process.env.REDIS_PASSWORD
+  });
+} else {
+  redis = new Redis(process.env.REDIS_URL);
+}
+
+export default redis;
