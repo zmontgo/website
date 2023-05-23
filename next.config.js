768131fa54648f6a912455a90072664b457a6970
Zachary Montgomery <zachmontgo@iodinedev.com>
2023-05-23 12:01:46 -0500

    Don't require bundle analyzer if not in production

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
 
 if (process.env.NODE_ENV === 'development') {
+  const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+  })
+  
   nextConfig.env = {
     SITE_URL: 'http://localhost:3000',
   }
