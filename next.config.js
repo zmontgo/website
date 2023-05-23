0a17b598501af5b2d44319717c6493c263efa4ff
Zachary Montgomery <zachmontgo@iodinedev.com>
2023-05-23 11:55:05 -0500

    Fix next config when not in production

--- next.config.js
+++ next.config.js
@@ -5,4 +5,20 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withBundleAnalyzer(nextConfig);
+if (process.env.NODE_ENV === 'production') {
+  nextConfig.images = {
+    domains: ['zachmontgomery.com'],
+  } 
+
+  nextConfig.env = {
+    SITE_URL: 'https://zachmontgomery.com',
+  }
+
+  module.exports = nextConfig;
+} else {
+  nextConfig.env = {
+    SITE_URL: 'http://localhost:3000',
+  }
+
+  module.exports = withBundleAnalyzer(nextConfig);
+}
